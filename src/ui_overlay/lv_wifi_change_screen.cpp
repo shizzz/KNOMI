5219537e0a89f84154f6ea93dc642bc4b97c7716
Kolmakov Sergey <kolmakov7@gmail.com>
2025-08-01 23:13:26 +0700

    change standby sceen. Disable welcome screen

--- src/ui_overlay/lv_wifi_change_screen.cpp
+++ src/ui_overlay/lv_wifi_change_screen.cpp
@@ -1,5 +1,6 @@
 #include "ui/ui.h"
 #include "knomi.h"
+#include "lv_overlay.h"
 
 static wifi_status_t lv_wifi_status = WIFI_STATUS_INIT;
 
@@ -13,7 +14,9 @@ static void lv_goto_wifi_screen(wifi_status_t state) {
     lv_obj_t * target = NULL;
     switch (state) {
         case WIFI_STATUS_ERROR:
-            target = ui_ScreenWelcome;
+            target = running_menu;
+            lv_gif_set_src(ui_img_main_gif, &gif_standby);
+            lv_obj_clear_flag(ui_ScreenMainGif, LV_OBJ_FLAG_CLICKABLE);
             break;
         case WIFI_STATUS_CONNECTING:
             target = ui_ScreenWIFIConnecting;
